13e03a752dfc58d63a1cae6271d01d1c56f89d74
Dr. Elizabeth Aslinger <elizabeth.aslinger@aya.yale.edu>
2023-10-24 11:34:17 -0500

    Update test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -6,34 +6,13 @@ class GuideRNATests:
     kwargs = {
         "assay": None, "assay_protein": None, "col_cell_type": "celltype",
         "col_gene_symbols": "gene_symbol", "col_sample_id": None, 
-        "col_batch": None, "col_perturbation": "perturbation", 
+        "col_perturbation": "perturbation", 
         "col_guide_rna": "perturbation", "col_num_umis": "UMI count", 
         "kws_process_guide_rna": dict(
-            feature_split=None, guide_split="_", key_control_patterns=[np.nan]), 
+            feature_split=None, guide_split="_", key_control_patterns=[np.nan], 
+            remove_multi_transfected=True), 
         "col_target_genes": "perturbation", "key_control": "Control", 
-        "key_treatment": "KO", "remove_multi_transfected": True}
-    # adata = cr.pp.create_object("data/adamson_2016_pilot.h5ad", **kwargs)
-    adata = pt.dt.adamson_2016_upr_perturb_seq()
-    out = cr.pp.filter_by_guide_counts(
-        adata, kwargs["col_guide_rna"], kwargs["col_num_umis"],
-        max_percent_umis_control_drop=75,
-        min_percent_umis=40, **kwargs["kws_process_guide_rna"], 
-        key_control="Control")
-
-    def test_output_processing_guides(self):
-        """Check that output format conforms to Crispr method expectations.""" 
-        col_guide_rna = GuideRNATests.kwargs["col_guide_rna"]
-        col_gp = cr.Crispr._columns_created["guide_percent"]
-        assert(isinstance(GuideRNATests.out, (tuple, set, list, np.ndarray)))
-        assert(len(GuideRNATests.out) == 2)
-        tg_info, feats_n = GuideRNATests.out
-        assert(f"{col_guide_rna}_list_filtered" in tg_info.columns)
-        assert(tg_info[f"{col_guide_rna}_list_filtered"].apply(
-            lambda x: isinstance(x, list)).all())  # all are lists
-        assert(col_gp in feats_n.columns), f"""
-        {col_gp} not in `cr.pp.filter_by_guide_counts` 
-        as expected by Crispr class methods.
-        Columns in 2nd element of output:\n{feats_n.columns}"""
+        "key_treatment": "KO"}
         
     # def test_name_processing(self):
         # cgrna = pd.read_excel("data/process_guide_rna/multi.xlsx", 
@@ -50,7 +29,39 @@ class GuideRNATests:
         #                         key_control_patterns=key_control_patterns,
         #                         key_control="Control")
                 
-        
+class CrisprObject:
+    """Object creation tests."""
+    
+    kwargs = {
+        "assay": None, "assay_protein": None, "col_cell_type": "celltype",
+        "col_gene_symbols": "gene_symbol", "col_sample_id": None, 
+        "col_perturbation": "perturbation", 
+        "col_guide_rna": "perturbation", "col_num_umis": "UMI count", 
+        "kws_process_guide_rna": dict(
+            feature_split=None, guide_split="_", key_control_patterns=[np.nan], 
+            remove_multi_transfected=True), 
+        "col_target_genes": "perturbation", "key_control": "Control", 
+        "key_treatment": "KO"}
+    # adata = cr.pp.create_object("data/adamson_2016_pilot.h5ad", **kwargs)
+    adata = pt.dt.adamson_2016_upr_perturb_seq()
+    # out = cr.pp.filter_by_guide_counts(
+    #     adata, kwargs["col_guide_rna"], kwargs["col_num_umis"],
+    #     max_percent_umis_control_drop=75,
+    #     min_percent_umis=40, **kwargs["kws_process_guide_rna"], 
+    #     key_control="Control")
+    self = cr.Crispr(adata, **kwargs)
+    
+	def test_attribute_presence:
+    """Ensure expected attributes are present."""
+    assert "adata" in dir(CrisprObject.self)
+    assert "rna" in dir(CrisprObject.self)
+    assert "uns" in dir(CrisprObject.self)
+    assert "obs" in dir(CrisprObject.self)
+    assert "_columns" in dir(CrisprObject.self)
+    assert "_layers" in dir(CrisprObject.self)
+    assert "_keys" in dir(CrisprObject.self)
+    
+    
 # class Preprocessing:
     
 #     def test_filtering(adata):
