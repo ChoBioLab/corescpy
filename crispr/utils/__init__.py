5ca0c221cccc6c4a64d6c4cafe75f666986028e0
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-02-04 20:42:23 -0500

    toppgene api

--- crispr/utils/__init__.py
+++ crispr/utils/__init__.py
@@ -6,10 +6,11 @@ from .display import (print_dictionary, make_printable_object,
 from .math import is_outlier
 from .data_manipulation import create_pseudobulk, create_condition_combo
 from .argument_manipulation import to_list, merge
-from .resources import get_markers_database
+from .resources import get_markers_database, get_topp_gene
 
 __all__ = [
     "print_dictionary", "make_printable_object", "print_pretty_dictionary",
     "explore_h5_file", "print_counts", "is_outlier", "create_pseudobulk",
-    "create_condition_combo", "to_list", "merge", "get_markers_database"
+    "create_condition_combo", "to_list", "merge",
+    "get_markers_database", "get_topp_gene"
 ]
