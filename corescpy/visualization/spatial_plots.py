a8df1b12532578e6f1be860d240f1645177841aa
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-03-13 14:59:24 -0400

    tangram deterministic label transfer (predicted cell types) + plotting fixes

--- corescpy/visualization/spatial_plots.py
+++ corescpy/visualization/spatial_plots.py
@@ -59,7 +59,10 @@ def plot_spatial(adata, color="leiden", col_segment=None, figsize=20,
                            library_id=libid, color=color, alt_var=cgs,
                            wspace=wspace), kwargs)  # keyword arguments
     kws["img_res_key"] = key_image if key_image else list(
-        ann.uns[spatial_key][libid]["images"].keys())[0]
+        ann.uns[spatial_key][libid if isinstance(libid, str) else libid[0]][
+            "images"].keys())[0]
+
+    # Plot
     try:
         with plt.rc_context({"figure.constrained_layout.use": True}):
             try:
@@ -68,7 +71,6 @@ def plot_spatial(adata, color="leiden", col_segment=None, figsize=20,
             except Exception:  # remove Leiden colors if => Squidpy bug
                 for c in color:
                     _ = ann.uns.pop(f"{c}_colors", None)
-                print(ann)
                 fig = sq.pl.spatial_segment(ann, col_segment, **kws) if (
                     col_segment) else sq.pl.spatial_scatter(ann, **kws)
     except Exception:
@@ -84,37 +86,91 @@ def plot_spatial(adata, color="leiden", col_segment=None, figsize=20,
 
 
 def plot_integration_spatial(adata_sp, adata_sp_new=None, adata_sc=None,
-                             col_cell_type=None, ad_map=None,
-                             df_compare=None, plot_genes=None):
+                             col_cell_type=None, ad_map=None, cmap="magma",
+                             df_compare=None, plot_genes=None, perc=0.01,
+                             col_annotation=None, figsize=20, **kwargs):
     """Plot integration (see `corescpy.pp.integrate_spatial`)."""
     figs = {}
     col_cell_type, col_cell_type_sp = [None, None] if (
         col_cell_type) is None else [col_cell_type, col_cell_type] if (
             isinstance(col_cell_type, str)) else col_cell_type
+    if isinstance(figsize, (int, float)):
+        figsize = (figsize, figsize)
     if adata_sc and col_cell_type:
-        tg.plot_cell_annotation_sc(adata_sp, list(pd.unique(adata_sc.obs[
-            col_cell_type])), perc=0.02)  # annotations spatial plot
+        # tg.plot_cell_annotation_sc(adata_sp, list(pd.unique(adata_sc.obs[
+        #     col_cell_type])), perc=0.02)  # annotations spatial plot
         if col_cell_type_sp not in adata_sp.obs:
             col_cell_type_sp = None  # if not present, ignore for plotting
         try:
             if col_cell_type_sp:
-                fig, axs = plt.subplots(1, 2, figsize=(20, 5))
+                fig, axs = plt.subplots(1, 2, figsize=figsize)
                 plot_spatial(adata_sp, color=col_cell_type_sp, ax=axs[0])
             sc.pl.umap(adata_sc, color=col_cell_type, size=10, frameon=False,
                        show=False if col_cell_type_sp else True,
-                       ax=axs[1] if col_cell_type_sp else None)  # UMAP
+                       ax=axs[1] if col_cell_type_sp else None,
+                       legend_loc="on data")  # UMAP
             plt.tight_layout()
-            figs["clusters"] = fig
+            figs["clusters"] = plt.gcf()
         except Exception:
+            figs["clusters"] = tb.format_exc()
             print(tb.format_exc(), "\n\n", "Plotting UMAPs/spatial failed!")
-    if ad_map:
+        try:  # plot integration-inferred spatial cell types
+            tmp, dfp, _ = cr.pp.construct_obs_spatial_integration(
+                adata_sp.copy(), adata_sc.copy(), col_cell_type, perc=perc,
+                suffix=None)  # spatial AnnData + cell type density columns
+            figs["spatial_density"] = plot_spatial(tmp, color=list(pd.unique(
+                adata_sc.obs[col_cell_type])), cmap=cmap)  # cell types plot
+        except Exception:
+            figs["clusters"] = tb.format_exc()
+            print(tb.format_exc(), "\n\n", "Plotting UMAPs/spatial failed!")
+    if ad_map is not None:  # plot training scores
         tg.plot_training_scores(ad_map, bins=20, alpha=0.5)  # train score
         figs["scores_training"] = plt.gcf()
-    if df_compare:
-        tg.plot_auc(df_compare)  # area under the curve
-        figs["auc"] = plt.gcf()
-    if plot_genes:
+    if df_compare is not None:  # plot AUC for predictions
+        figs["auc"] = plot_auc_spatial_integration(
+            df_compare, title="Prediction on Test Transcriptome")  # AUC
+        plt.gcf()
+    if plot_genes is not None and adata_sp_new is not None:  # plot GEX
         tg.plot_genes_sc(plot_genes, adata_measured=adata_sp,
                          adata_predicted=adata_sp_new, perc=0.02)
         figs["genes"] = plt.gcf()
+    if col_annotation is not None:  # predicted cell types spatial plot
+        figs["spatial"] = plot_spatial(
+            adata_sp, color=col_annotation, **kwargs)  # plot predicted types
     return figs
+
+
+def plot_auc_spatial_integration(df_all_genes, test_genes=None, title=""):
+    """
+        Plots auc curve which is used to evaluate model performance.
+        Fixed version of Tangram's `plot_auc` function.
+
+    Args:
+        df_all_genes (Pandas DataFrame): Returned by
+            `tangram.compare_spatial_geneexp(adata_ge, adata_sp)`.
+        test_genes (list): List of test genes, if not given,
+            test_genes will be set to genes where 'is_training'
+            field is False.
+
+    Returns:
+        None
+    """
+    metric_dict, ((pol_xs, pol_ys), (x_s, y_s)) = tg.utils.eval_metric(
+        df_all_genes, test_genes)
+    fig = plt.figure()
+    plt.figure(figsize=(6, 5))
+    plt.plot(pol_xs, pol_ys, c="r")
+    plt.scatter(x_s, y_s, alpha=0.5, edgecolors="face")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.gca().set_aspect(.5)
+    plt.xlabel("Score")
+    plt.ylabel("Spatial Sparsity")
+    plt.tick_params(axis="both", labelsize=8)
+    plt.title(title)
+    textstr = "auc_score={}".format(np.round(metric_dict["auc_score"], 3))
+    props = dict(boxstyle="round", facecolor="wheat", alpha=0.3)
+    # place a text box in upper left in axes coords
+    plt.text(0.03, 0.1, textstr, fontsize=12,
+             verticalalignment="top", bbox=props)
+    return fig
