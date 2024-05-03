0655f38c435cf69817a10b7b9d4a186c75aaea3a
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-05-03 12:20:12 -0400

    add to init

--- corescpy/analysis/__init__.py
+++ corescpy/analysis/__init__.py
@@ -7,7 +7,8 @@ from .perturbations import (
     compute_distance, perform_gsea, perform_gsea_pt,
     perform_pathway_interference, perform_dea, calculate_dea_deseq2)
 from .clustering import (cluster, find_marker_genes, make_marker_genes_df,
-                         perform_celltypist, annotate_by_markers)
+                         perform_celltypist, annotate_by_markers,
+                         print_marker_info)
 from .composition import analyze_composition
 from .communication import analyze_receptor_ligand, analyze_causal_network
 
@@ -16,7 +17,7 @@ __all__ = [
     "perform_differential_prioritization", "compute_distance",
     "perform_gsea", "perform_gsea_pt", "perform_pathway_interference",
     "perform_dea", "calculate_dea_deseq2", "cluster",
-    "find_marker_genes", "make_marker_genes_df",
+    "find_marker_genes", "make_marker_genes_df", "print_marker_info",
     "perform_celltypist", "annotate_by_markers", "analyze_composition",
     "analyze_receptor_ligand", "analyze_causal_network"
 ]
