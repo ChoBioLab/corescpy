9a54850777d76b7cb9b35cc65915f8f76f2a648b
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-05-06 13:43:18 -0400

    start fixing outlier-based auto-preprocess: can have above, below, or both (e.g., want maximum mt but not minimum)

--- corescpy/utils/math.py
+++ corescpy/utils/math.py
@@ -12,9 +12,21 @@ import numpy as np
 
 
 def is_outlier(data, column, nmads: int):
-    """Determine outliers (from SC Best Practices)."""
+    """Determine outliers [below, above median]."""
+    # From SC Best Practices
     metric = data[column]
-    outlier = (metric < np.median(metric) - nmads * median_abs_deviation(
-        metric)) | (np.median(metric) + nmads * median_abs_deviation(
-            metric) < metric)
+    if isinstance(nmads, (int, float)):
+        nmads = [nmads, nmads]
+    if nmads[0] is not None and nmads[1] is not None:
+        outlier = (metric < np.median(metric) - nmads[
+            0] * median_abs_deviation(metric)) | (np.median(
+                metric) + nmads[1] * median_abs_deviation(metric) < metric)
+    elif nmads[0] is None:
+        outlier = (np.median(metric) + nmads[1] * median_abs_deviation(
+                metric) < metric)
+    elif nmads[1] is None:
+        outlier = (metric < np.median(metric) - nmads[
+            0] * median_abs_deviation(metric))
+    else:
+        raise ValueError("Can't have None for both `nmads` elements.")
     return outlier
