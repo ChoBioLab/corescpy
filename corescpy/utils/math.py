fix calculation of threshold in outlier-based pp/qc

corescpy/utils/math.py
@@ -11,20 +11,24 @@ from scipy.stats import median_abs_deviation
 import numpy as np
 
 
-def is_outlier(data, column, nmads: int):
+def is_outlier(data, column, nmads):
     """Determine outliers [below, above median]."""
     # From SC Best Practices
     metric = data[column]
     if isinstance(nmads, (int, float)):
         nmads = [nmads, nmads]
     mad = median_abs_deviation(metric)
+    thresh = [np.median(metric) - nmads[0] * mad,
+              np.median(metric) + nmads[1] * mad]  # minimum, maximum
     if nmads[0] is not None and nmads[1] is not None:
-        outlier = (metric < np.median(metric) - nmads[
-            0] * mad) | (np.median(metric) + nmads[1] * mad < metric)
-    elif nmads[0] is None:
+        outlier = (metric < thresh[0]) | (thresh[1] < metric)
+    elif nmads[0] is None:  # not calculating based on a minimum
+        thresh[0] = None  # no minimum
         outlier = (np.median(metric) + nmads[1] * mad < metric)
-    elif nmads[1] is None:
-        outlier = metric < np.median(metric) - nmads[0] * mad
+        outlier = (metric < thresh[0]) | (thresh[1] < metric)
+    elif nmads[1] is None:  # not calculating based on a maximum
+        thresh[1] = None  # no maximum
+        outlier = metric < thresh[0]
     else:
         raise ValueError("Can't have None for both `nmads` elements.")
-    return outlier, [i * mad if i else None for i in nmads]  # y/n, threshold
+    return outlier, thresh  # y/n, threshold