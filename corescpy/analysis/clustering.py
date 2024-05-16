aeb4484e4f8af8d69959fb9c1184cb812b9b269d
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-05-16 18:03:22 -0400

    minor display change

--- corescpy/analysis/clustering.py
+++ corescpy/analysis/clustering.py
@@ -459,7 +459,7 @@ def print_marker_info(adata, key_cluster, assign, col_cell_type=None,
         n_exp = n_exp.assign(Percent_Total=100 * n_exp[key_cluster] / n_exp[
             "Total"]).sort_values("Percent_Total", ascending=False)
         n_exp = n_exp.join(n_exp.groupby("Gene").apply(lambda x: assign.loc[
-            x.name].iloc[0] if len(
+            x.name].group.iloc[0] if len(
                 assign.loc[x.name].group.unique()) == 1 else ", ".join(
                     list(assign.loc[x.name].group.unique()))).to_frame(
                         "Cell Types"))  # add annotations to n_exp
