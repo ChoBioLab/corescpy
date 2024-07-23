a656734fbcd9bc78669606fa06e9ffaca2027747
Elizabeth <elizabeth.aslinger@aya.yale.edu>
2024-07-23 12:50:42 -0400

    fix imports

--- corescpy/__init__.py
+++ corescpy/__init__.py
@@ -7,9 +7,9 @@ from . import processing as pp
 from . import analysis as ax
 from . import visualization as pl
 from . import class_crispr, class_sc, class_spatial, defaults
-from .class_sc import Omics
-from .class_crispr import Crispr
-from .class_spatial import Spatial
+# from .class_sc import Omics
+# from .class_crispr import Crispr
+# from .class_spatial import Spatial
 
 mod = ["ax", "pl", "pp", "tl", "Omics", "Crispr", "Spatial"]
 sys.modules.update({f"{__name__}.{m}": globals()[m] for m in mod})
